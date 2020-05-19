216dbbd55eb38d920dd8b6a3e4b9565771183fb6
Max Magee <maxmagee@users.noreply.github.com>
2020-05-19 17:56:02 -0500

    Added header buttons for navigation.

--- screens/ActiveToDoListScreen.js
+++ screens/ActiveToDoListScreen.js
@@ -1,6 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Button, StyleSheet, Text, View } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { DrawerActions } from "react-navigation-drawer";
+import CustomHeaderButton from "../components/ui/CustomHeaderButton";
 
 const ActiveToDoListScreen = (props) => {
   const { navigation } = props;
@@ -24,6 +27,34 @@ ActiveToDoListScreen.propTypes = {
   }).isRequired,
 };
 ActiveToDoListScreen.defaultProps = {};
+
+ActiveToDoListScreen.navigationOptions = (navigationData) => {
+  const goToCreateScreenHandler = () => {
+    navigationData.navigation.navigate("CreateToDo");
+  };
+
+  const openDrawerHandler = () => {
+    navigationData.navigation.dispatch(DrawerActions.openDrawer());
+  };
+
+  return {
+    headerLeft: () => {
+      return (
+        <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+          <Item iconName="ios-menu" onPress={openDrawerHandler} title="Menu" />
+        </HeaderButtons>
+      );
+    },
+    headerRight: () => {
+      return (
+        <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+          <Item iconName="ios-add" onPress={goToCreateScreenHandler} title="Create" />
+        </HeaderButtons>
+      );
+    },
+    headerTitle: "Active Tasks",
+  };
+};
 
 const styles = StyleSheet.create({
   centeredScreen: {
