479896bfcc5020bc2dffaffc8943e0ffa41a3b55
Max Magee <maxmagee@users.noreply.github.com>
2020-05-20 18:02:24 -0500

    #21: Mostly have date picker working. Still needs work.

--- screens/CreateToDoScreen.js
+++ screens/CreateToDoScreen.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { StyleSheet, View } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { Button, StyleSheet, View } from "react-native";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 
@@ -10,8 +11,14 @@ import CustomTextInput from "../components/ui/CustomTextInput";
 import colors from "../constants/colors";
 import * as taskActions from "../store/actions/tasks";
 
+const moment = require("moment");
+
+const today = new Date();
+
 const CreateToDoScreen = (props) => {
   const { navigation } = props;
+  const [isDatePickerVisible, setIsDatePickerVisible] = useState(false);
+  const [dueDate, setDueDate] = useState(today);
   const dispatch = useDispatch();
 
   const { register, handleSubmit, setValue } = useForm();
@@ -19,10 +26,25 @@ const CreateToDoScreen = (props) => {
   useEffect(() => {
     register("name");
     register("description");
+    register("dueDate");
   }, [register]);
 
+  const hideDatePicker = () => {
+    setIsDatePickerVisible(false);
+  };
+
+  const showDatePicker = () => {
+    setIsDatePickerVisible(true);
+  };
+
+  const dateConfirmedHandler = (date) => {
+    setDueDate(date);
+    setValue("dueDate", date.toString());
+    hideDatePicker();
+  };
+
   const onSubmitHandler = (data) => {
-    dispatch(taskActions.createTask(data.name, data.description, new Date()));
+    dispatch(taskActions.createTask(data.name, data.description, data.dueDate || today));
     navigation.goBack();
   };
 
@@ -42,6 +64,22 @@ const CreateToDoScreen = (props) => {
           setValue("description", text);
         }}
       />
+      <CustomTextInput
+        onFocus={showDatePicker}
+        label="Due Date"
+        editable={false}
+        value={moment(dueDate).format("ddd LL")}
+      />
+      <View>
+        <Button color={colors.dark.blue} title="Change Due Date" onPress={showDatePicker} />
+        <DateTimePickerModal
+          mode="date"
+          minimumDate={today}
+          isVisible={isDatePickerVisible}
+          onConfirm={dateConfirmedHandler}
+          onCancel={hideDatePicker}
+        />
+      </View>
       <View style={styles.buttonContainer}>
         <CallToActionButton label="Submit" onPress={handleSubmit(onSubmitHandler)} />
       </View>
