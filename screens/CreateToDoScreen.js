cb4052766733cf8730cc116b6a9cfb6b73092bd7
Max Magee <maxmagee@users.noreply.github.com>
2020-05-20 16:27:03 -0500

    #21: Basic form layout for create implemented.

--- screens/CreateToDoScreen.js
+++ screens/CreateToDoScreen.js
@@ -2,16 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 import { StyleSheet, View } from "react-native";
 
-import DefaultText from "../components/ui/DefaultText";
+import CallToActionButton from "../components/ui/CallToActionButton";
+import CustomTextInput from "../components/ui/CustomTextInput";
 
 import colors from "../constants/colors";
 
 const CreateToDoScreen = (props) => {
   const { navigation } = props;
 
+  const onSubmitHandler = () => {
+    console.log("submit pressed");
+  };
+
   return (
     <View style={styles.centeredScreen}>
-      <DefaultText>Create ToDo</DefaultText>
+      <CustomTextInput label="Name" />
+      <CustomTextInput label="Description" style={styles.textArea} multiline />
+      <View style={styles.buttonContainer}>
+        <CallToActionButton label="Submit" onPress={onSubmitHandler} />
+      </View>
     </View>
   );
 };
@@ -23,12 +32,24 @@ CreateToDoScreen.propTypes = {
 };
 CreateToDoScreen.defaultProps = {};
 
+CreateToDoScreen.navigationOptions = {
+  headerTitle: "Create",
+};
+
 const styles = StyleSheet.create({
+  buttonContainer: {
+    marginTop: 20,
+    width: "100%",
+  },
   centeredScreen: {
     alignItems: "center",
     backgroundColor: colors.dark.systemGray6,
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
+    padding: 20,
+  },
+  textArea: {
+    height: 80,
   },
 });
 
