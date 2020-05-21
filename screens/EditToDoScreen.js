d6394997ce587a6dd274faf1ed324a3ae6e88ffb
Max Magee <maxmagee@users.noreply.github.com>
2020-05-20 21:01:15 -0500

    #24: Users can now edit tasks.

--- screens/EditToDoScreen.js
+++ screens/EditToDoScreen.js
@@ -1,34 +1,126 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { StyleSheet, View } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { Button, Keyboard, ScrollView, StyleSheet, View } from "react-native";
+import { Controller, useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
 
-import DefaultText from "../components/ui/DefaultText";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import CallToActionButton from "../components/ui/CallToActionButton";
+import CustomTextInput from "../components/ui/CustomTextInput";
 
 import colors from "../constants/colors";
+import * as taskActions from "../store/actions/tasks";
+
+const moment = require("moment");
+
+const today = new Date();
 
 const EditToDoScreen = (props) => {
   const { navigation } = props;
+  const task = navigation.getParam("task");
+  const [isDatePickerVisible, setIsDatePickerVisible] = useState(false);
+  const [dueDate, setDueDate] = useState(today);
+  const dispatch = useDispatch();
+
+  const { control, setValue, handleSubmit } = useForm();
+
+  const hideDatePicker = () => {
+    setIsDatePickerVisible(false);
+  };
+
+  const showDatePicker = () => {
+    setIsDatePickerVisible(true);
+  };
+
+  const dateConfirmedHandler = (date) => {
+    // Set local state variable so we can display a different value
+    setDueDate(date);
+    // Use the display version in the text input
+    setValue("dueDate", moment(date).format("ddd LL"));
+    hideDatePicker();
+  };
+
+  const onSubmitHandler = (data) => {
+    dispatch(taskActions.createTask(data.name, data.description, dueDate));
+    navigation.goBack();
+  };
 
   return (
-    <View style={styles.centeredScreen}>
-      <DefaultText>Edit ToDo</DefaultText>
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <ScrollView style={styles.scrollView}>
+        <View behavior="height" style={styles.centeredScreen}>
+          <Controller
+            as={<CustomTextInput label="Name" />}
+            control={control}
+            name="name"
+            onChange={(args) => args[0].nativeEvent.text}
+            defaultValue={task.name}
+          />
+          <Controller
+            as={<CustomTextInput label="Description" multiline style={styles.textArea} />}
+            control={control}
+            name="description"
+            onChange={(args) => args[0].nativeEvent.text}
+            defaultValue={task.description}
+          />
+          <Controller
+            as={<CustomTextInput label="Due Date" editable={false} onFocus={showDatePicker} />}
+            control={control}
+            name="dueDate"
+            onChange={(args) => args[0].nativeEvent.text}
+            defaultValue={moment(task.dueDate).format("ddd LL")}
+          />
+          <View>
+            <Button color={colors.dark.blue} title="Change Due Date" onPress={showDatePicker} />
+            <DateTimePickerModal
+              mode="date"
+              minimumDate={today}
+              isVisible={isDatePickerVisible}
+              onConfirm={dateConfirmedHandler}
+              onCancel={hideDatePicker}
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <CallToActionButton label="Submit" onPress={handleSubmit(onSubmitHandler)} />
+          </View>
+        </View>
+      </ScrollView>
+    </TouchableWithoutFeedback>
   );
 };
 
 EditToDoScreen.propTypes = {
   navigation: PropTypes.shape({
-    navigate: PropTypes.func.isRequired,
+    getParam: PropTypes.func.isRequired,
+    goBack: PropTypes.func.isRequired,
   }).isRequired,
 };
 EditToDoScreen.defaultProps = {};
 
+EditToDoScreen.navigationOptions = {
+  headerTitle: "Create",
+};
+
 const styles = StyleSheet.create({
+  buttonContainer: {
+    marginTop: 20,
+    width: "100%",
+  },
   centeredScreen: {
     alignItems: "center",
     backgroundColor: colors.dark.systemGray6,
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  scrollView: {
+    backgroundColor: colors.dark.systemGray6,
+    height: "100%",
+  },
+  textArea: {
+    height: 80,
   },
 });
 
