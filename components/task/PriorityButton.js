489c8d7249fd3b80795a4864d0a98b60113d2cfa
Max Magee <maxmagee@users.noreply.github.com>
2020-05-21 15:50:22 -0500

    #13: Created a PriorityButton component.

---
+++ components/task/PriorityButton.js
@@ -0,0 +1,63 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+
+import DefaultText from "../ui/DefaultText";
+
+import colors from "../../constants/colors";
+
+const PriorityButton = (props) => {
+  const { isActive, onPress, priority } = props;
+  const fillColor = priority.color;
+  const buttonStyle = isActive ? { ...styles.button, ...styles.activeButton } : styles.button;
+
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.container}>
+        <View style={{ ...buttonStyle, backgroundColor: fillColor }} />
+        <DefaultText style={styles.label}> - {priority.name}</DefaultText>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+PriorityButton.propTypes = {
+  isActive: PropTypes.bool.isRequired,
+  onPress: PropTypes.func.isRequired,
+  priority: PropTypes.shape({
+    color: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+PriorityButton.defaultProps = {};
+
+const styles = StyleSheet.create({
+  activeButton: {
+    shadowColor: "white",
+    shadowOffset: {
+      height: 0,
+      width: 0,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 7,
+  },
+  button: {
+    backgroundColor: colors.dark.red,
+    borderRadius: 10,
+    borderWidth: 1,
+    height: 20,
+    width: 20,
+  },
+  container: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  label: {
+    fontFamily: "open-sans-bold",
+  },
+});
+
+export default PriorityButton;
