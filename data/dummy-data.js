15d587d77b55408ed5417639e22b8d14daa5adee
Max Magee <maxmagee@users.noreply.github.com>
2020-05-21 22:20:03 -0500

    #31: Updated the dummy-data.

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -1,231 +1,61 @@
 import Task from "../models/task";
-import utils from "../utils/utils";
 
 const TASKS = [
   new Task(
     "t1",
     "u1",
     "high",
-    "Wash the dishes",
-    "The sink is full of dishes, clean them.",
-    utils.getRandomDate(),
+    "Review Challenge Submission",
+    "Look over the React Native challenge submission by Max Magee.",
+    new Date(2020, 4, 21),
     true
   ),
   new Task(
     "t2",
     "u1",
     "medium",
-    "Take out the trash",
-    "Garbage collection is tomorrow morning.",
-    utils.getRandomDate(),
+    "Schedule Technical Interview",
+    "Schedule a time to conduct the technical in-depth interview with the candidate.",
+    new Date(2020, 4, 25),
     true
   ),
   new Task(
     "t3",
     "u1",
     "low",
-    "Sweep the floor",
-    "The dog brought in a lot of footprints last night.",
-    utils.getRandomDate(),
-    false,
-    utils.getRandomDate()
+    "Conduct Technical Interview",
+    "Conduct the technical in-depth interview to further evaluate Max's fitness for the role.",
+    new Date(2020, 4, 26),
+    true
   ),
   //
   new Task(
     "t4",
     "u1",
     null,
-    "Wash the dishes",
-    "The sink is full of dishes, clean them.",
-    utils.getRandomDate(),
+    "Conduct Cultural Interview",
+    "It's important that he will work well within the team environment.",
+    new Date(2020, 4, 27),
     true
   ),
   new Task(
     "t5",
     "u1",
-    null,
-    "Take out the trash",
-    "Garbage collection is tomorrow morning.",
-    utils.getRandomDate(),
+    "high",
+    "Make Max An Offer",
+    "We want Max on the team, extend an offer to him as soon as possible.",
+    new Date(2020, 4, 27),
     true
   ),
   new Task(
     "t6",
     "u1",
-    null,
-    "Sweep the floor",
-    "The dog brought in a lot of footprints last night.",
-    utils.getRandomDate(),
+    "high",
+    "Receive Max's Challenge Submission",
+    "Check to see if Max has sent in his challenge submission.",
+    new Date(2020, 4, 21),
     false,
-    utils.getRandomDate()
-  ),
-  new Task(
-    "t7",
-    "u1",
-    null,
-    "Wash the dishes",
-    "The sink is full of dishes, clean them.",
-    utils.getRandomDate(),
-    true
-  ),
-  new Task(
-    "t8",
-    "u1",
-    null,
-    "Take out the trash",
-    "Garbage collection is tomorrow morning.",
-    utils.getRandomDate(),
-    true
-  ),
-  new Task(
-    "t9",
-    "u1",
-    null,
-    "Sweep the floor",
-    "The dog brought in a lot of footprints last night.",
-    utils.getRandomDate(),
-    false,
-    utils.getRandomDate()
-  ),
-  new Task(
-    "t10",
-    "u1",
-    null,
-    "Wash the dishes",
-    "The sink is full of dishes, clean them.",
-    utils.getRandomDate(),
-    true
-  ),
-  new Task(
-    "t11",
-    "u1",
-    null,
-    "Take out the trash",
-    "Garbage collection is tomorrow morning.",
-    utils.getRandomDate(),
-    true
-  ),
-  new Task(
-    "t12",
-    "u1",
-    null,
-    "Sweep the floor",
-    "The dog brought in a lot of footprints last night.",
-    utils.getRandomDate(),
-    false,
-    utils.getRandomDate()
-  ),
-  new Task(
-    "t13",
-    "u1",
-    null,
-    "Wash the dishes",
-    "The sink is full of dishes, clean them.",
-    utils.getRandomDate(),
-    true
-  ),
-  new Task(
-    "t14",
-    "u1",
-    null,
-    "Take out the trash",
-    "Garbage collection is tomorrow morning.",
-    utils.getRandomDate(),
-    true
-  ),
-  new Task(
-    "t15",
-    "u1",
-    null,
-    "Sweep the floor",
-    "The dog brought in a lot of footprints last night.",
-    utils.getRandomDate(),
-    false,
-    utils.getRandomDate()
-  ),
-  new Task(
-    "t16",
-    "u1",
-    null,
-    "Wash the dishes",
-    "The sink is full of dishes, clean them.",
-    utils.getRandomDate(),
-    true
-  ),
-  new Task(
-    "t17",
-    "u1",
-    null,
-    "Take out the trash",
-    "Garbage collection is tomorrow morning.",
-    utils.getRandomDate(),
-    true
-  ),
-  new Task(
-    "t18",
-    "u1",
-    null,
-    "Sweep the floor",
-    "The dog brought in a lot of footprints last night.",
-    utils.getRandomDate(),
-    false,
-    utils.getRandomDate()
-  ),
-  new Task(
-    "t24",
-    "u1",
-    null,
-    "Wash the dishes",
-    "The sink is full of dishes, clean them.",
-    utils.getRandomDate(),
-    true
-  ),
-  new Task(
-    "t19",
-    "u1",
-    null,
-    "Take out the trash",
-    "Garbage collection is tomorrow morning.",
-    utils.getRandomDate(),
-    true
-  ),
-  new Task(
-    "t20",
-    "u1",
-    null,
-    "Sweep the floor",
-    "The dog brought in a lot of footprints last night.",
-    utils.getRandomDate(),
-    false,
-    utils.getRandomDate()
-  ),
-  new Task(
-    "t21",
-    "u1",
-    null,
-    "Wash the dishes",
-    "The sink is full of dishes, clean them.",
-    utils.getRandomDate(),
-    true
-  ),
-  new Task(
-    "t22",
-    "u1",
-    null,
-    "Take out the trash",
-    "Garbage collection is tomorrow morning.",
-    utils.getRandomDate(),
-    true
-  ),
-  new Task(
-    "t23",
-    "u1",
-    null,
-    "Sweep the floor",
-    "The dog brought in a lot of footprints last night.",
-    utils.getRandomDate(),
-    false,
-    utils.getRandomDate()
+    new Date(2020, 4, 21)
   ),
 ];
 
